eccb7fd77f381cf57ffa9627029304b86f175f8f
Leonardo Cserny <leo.cserny@gmail.com>
2021-02-04 19:24:30 +0200

    [2] adds basic servers list UI

---
+++ app/src/main/java/io/github/lcserny/shutdownapp/ServersListActivity.java
@@ -0,0 +1,13 @@
+package io.github.lcserny.shutdownapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ServersListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servers_list);
+    }
+}
