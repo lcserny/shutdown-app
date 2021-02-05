308f8f6b9508b2cdd43bd19e5339a55fcc5475e4
Leonardo Cserny <leonardo.cserny@endava.com>
2021-02-05 10:37:44 +0200

    [2] improves layouts

--- app/src/main/java/io/github/lcserny/shutdownapp/MainActivity.java
+++ app/src/main/java/io/github/lcserny/shutdownapp/MainActivity.java
@@ -3,11 +3,11 @@ package io.github.lcserny.shutdownapp;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ScanActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scan);
+        setContentView(R.layout.activity_main);
     }
 }
